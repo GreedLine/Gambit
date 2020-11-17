83b8f07f4a958a1ad5eefc2de709a1ed2d94c42c
GreedLine <greedline74@gmail.com>
2020-11-17 18:42:45 +0500

    Прокрутка сайдбара, мобильная адаптация, правка вёрстки с пиксельперфект

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
+import $ from 'jquery';
 import './js/js_1.js';
 import './js/js_2.js';
 
 import img from '../src/assets/img/favicon.png';
 import './scss/main.scss';
 import logotype from '../src/assets/logotype.png';
+
+const saes = document.getElementById('sa-es');
+
+function checkWidth() {
+    if (document.documentElement.clientWidth < 1300) {
+        document.getElementById('sa-es').innerHTML = 'SA ES';
+    }
+    if (document.documentElement.clientWidth > 1300) {
+        document.getElementById('sa-es').innerHTML = 'San Andreas Emergency Services';
+    }
+};
+
+function scrollSidebar() {
+    let y = getBodyScrollTop();
+    let res = 190 - y;
+    if (y >= 150) {
+        $('#sidebar').css({top: '40px',})
+    }
+    else{
+        $('#sidebar').css({top: res})
+    }
+
+
+}
+
+function getBodyScrollTop() {
+    return self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop);
+}
+
+$(window).scroll(() => scrollSidebar());
+$(window).resize(() => checkWidth());
+$(document).ready(() => {
+    scrollSidebar();
+    checkWidth();
+});
+
+
